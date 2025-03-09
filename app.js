786ea61288a336ee96d7821b37405dd01df36de5
CoolShivas <shivas2710cool00@gmail.com>
2025-03-09 16:55:25 +0530

    Use of export.property method.

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
-const add = require("./math");
-const sub = require("./math");
+// const add = require("./math");
+// const sub = require("./math");
 
-console.log(add(5,3)); // 8
-console.log(sub(5,3)); // 2
+// console.log(add(5,3)); // 8
+// console.log(sub(5,3)); // 2
 
 // // If we move to terminal for the result. We are expecting the result should be for the first console i.e, addition be 8 and second console i.e, subtraction be 2.
 
 // // But, we are getting the output on the node terminal be 2 and 2. Because, it is considering the final console or the last console only i.e, subtraction not the addition.
+
+
+// // To over come from such type of thing we have to use the object method and then destructing method.
+
+
+const {add , sub} = require("./math");
+
+console.log(add(5,3)); // 8
+console.log(sub(5,3)); // 2
+
+//////***************************************************************************************** */
--- math.js
+++ math.js
@@ -7,5 +7,30 @@ const sub = (a, b) => {
 };
 
 
-module.exports = add;
-module.exports = sub;
+// module.exports = add;
+// module.exports = sub;
+
+// // module.exports is a single object or value
+// // When you reassign module.exports(e.g., module.exports = add), it completely replaces whatever was the previously assigned.
+
+// If you reassign it again (module.exports = sub), the previous value (add) is replace with sub.
+
+// Avoid mixing module.exports and exports.property incorrectly. If you reassign module.exports, it will override any previous exports.property assignments.
+
+
+
+////////***************************************************************************************** */
+
+
+
+// // // Use consistent syntax for clarity:
+// // // Assign properties individually with module.exports.property.
+// // // OR
+// // // Assign everything at once with module.exports = {...}
+
+module.exports.add = add;
+module.exports.sub = sub;
+
+
+
+////////***************************************************************************************** */
