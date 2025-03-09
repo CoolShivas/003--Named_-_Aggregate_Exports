08f88bedc6785a890ddae9809a62c9921e34e00d
CoolShivas <shivas2710cool00@gmail.com>
2025-03-09 16:16:31 +0530

    Starting of Node.js Named & Aggregate Exports

---
+++ theory.js
@@ -0,0 +1,25 @@
+/***
+ * 
+ * * * * Named Export :-
+ * 
+ * 
+ * * *  Key Takeaways :-
+ * 
+    • Avoid mixing module.exports and exports.property incorrectly. 
+    
+    If you reassign module.exports, it will override any previous exports.property assignments.
+
+    • Use consistent syntax for clarity:
+
+    • Assign everything at once with module.exports = { ... }.
+
+    • Or assign properties individually with module.exports.property
+
+
+
+//////////--------------------------------------------------------------------------------------------------------------
+
+ * 
+ * 
+ * 
+ */
