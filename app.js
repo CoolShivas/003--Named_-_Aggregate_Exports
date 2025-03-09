50ce840b6bdda920d47f90bd0525761570703db6
CoolShivas <shivas2710cool00@gmail.com>
2025-03-09 16:28:26 +0530

    Formation of app.js and math.js

---
+++ app.js
@@ -0,0 +1,9 @@
+const add = require("./math");
+const sub = require("./math");
+
+console.log(add(5,3)); // 8
+console.log(sub(5,3)); // 2
+
+// // If we move to terminal for the result. We are expecting the result should be for the first console i.e, addition be 8 and second console i.e, subtraction be 2.
+
+// // But, we are getting the output on the node terminal be 2 and 2. Because, it is considering the final console or the last console only i.e, subtraction not the addition.
