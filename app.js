8ad66bd21ed85b5e304a616a863263eddc8361d2
CoolShivas <shivas2710cool00@gmail.com>
2025-03-09 17:22:02 +0530

    Use of Object and Destructuring Method.

--- app.js
+++ app.js
@@ -9,12 +9,28 @@
 // // But, we are getting the output on the node terminal be 2 and 2. Because, it is considering the final console or the last console only i.e, subtraction not the addition.
 
 
+//////***************************************************************************************** */
+
+
 // // To over come from such type of thing we have to use the object method and then destructing method.
 
 
-const {add , sub} = require("./math");
+// const {add , sub} = require("./math");
 
-console.log(add(5,3)); // 8
-console.log(sub(5,3)); // 2
+// console.log(add(5,3)); // 8
+// console.log(sub(5,3)); // 2
+
 
 //////***************************************************************************************** */
+
+
+// // Here, we know the files data coming from math.js that's why we require and destructure all by there name such as add, sub and all;
+// // If we don't know the names such as add, sub and all. Then have a look on the last one;
+
+const {addition , subtraction, multiply, division , PI} = require("./math");
+
+console.log(addition(5,3));     // 8
+console.log(subtraction(5,3));  // 2
+console.log(multiply(5,3));     // 15
+console.log(division(5,3));     // 1.6666666666666667
+console.log(PI);                // 3.214
--- math.js
+++ math.js
@@ -1,36 +1,59 @@
-const add = (a, b) => {
-    return a + b;
+// const add = (a, b) => {
+//     return a + b;
+// };
+
+// const sub = (a, b) => {
+//     return a - b;
+// };
+
+
+// // module.exports = add;
+// // module.exports = sub;
+
+// // // module.exports is a single object or value
+// // // When you reassign module.exports(e.g., module.exports = add), it completely replaces whatever was the previously assigned.
+
+// // If you reassign it again (module.exports = sub), the previous value (add) is replace with sub.
+
+// // Avoid mixing module.exports and exports.property incorrectly. If you reassign module.exports, it will override any previous exports.property assignments.
+
+
+
+// ////////***************************************************************************************** */
+
+
+
+// // // // Use consistent syntax for clarity:
+// // // // Assign properties individually with module.exports.property.
+// // // // OR
+// // // // Assign everything at once with module.exports = {...}
+
+// module.exports.add = add;
+// module.exports.sub = sub;
+
+
+
+// ////////***************************************************************************************** */
+
+
+
+const addition = (a, b) => {
+    return (a + b);
 };
 
-const sub = (a, b) => {
-    return a - b;
+const subtraction = (a, b) => {
+    return (a - b);
 };
 
+const multiply = (a, b) => {
+    return (a * b);
+};
 
-// module.exports = add;
-// module.exports = sub;
+const division = (a, b) => {
+    return (a / b);
+};
 
-// // module.exports is a single object or value
-// // When you reassign module.exports(e.g., module.exports = add), it completely replaces whatever was the previously assigned.
-
-// If you reassign it again (module.exports = sub), the previous value (add) is replace with sub.
-
-// Avoid mixing module.exports and exports.property incorrectly. If you reassign module.exports, it will override any previous exports.property assignments.
-
+const PI = 3.214;
 
 
-////////***************************************************************************************** */
-
-
-
-// // // Use consistent syntax for clarity:
-// // // Assign properties individually with module.exports.property.
-// // // OR
-// // // Assign everything at once with module.exports = {...}
-
-module.exports.add = add;
-module.exports.sub = sub;
-
-
-
-////////***************************************************************************************** */
+module.exports = {addition, subtraction, multiply, division, PI};
